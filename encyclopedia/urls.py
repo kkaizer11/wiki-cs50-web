bfc2cffeb4deebede2198165e75b509ce9aa6588
KKaizer <matheus.kaizer@hotmail.com>
2023-02-09 19:16:25 -0300

    search added

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:entry>", views.entry_page, name="wiki")
+    path("wiki/<str:entry>", views.entry_page, name="wiki"),
+    path("search/", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from markdown2 import Markdown
 
 from . import util
@@ -14,3 +14,14 @@ def entry_page(request, entry):
             return render(request, "encyclopedia/error.html", {})
         page = util.get_entry(entry)
         return render(request, "encyclopedia/wiki.html", {"title":entry, "content": Markdown().convert(page)})
+
+def search(request):
+    query = request.GET.get('q')
+    if query is None or query.isspace()==True:
+        return render(request, "encyclopedia/search.html", {"query": query, "matches": ""})
+    pages = util.list_entries()
+    matches = [
+    result
+    for result in pages
+    if query.upper() in result.upper()]
+    return render(request, "encyclopedia/search.html", {"query": query, "matches": matches})
